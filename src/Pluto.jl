3c918e97b0b0f22f3935ca7a8a8a3f4d8ccc499c
Fons van der Plas <fonsvdplas@gmail.com>
2026-03-19 10:07:24 +0100

    Update Pluto.jl

--- src/Pluto.jl
+++ src/Pluto.jl
@@ -5,8 +5,8 @@ Start a notebook server using:
 julia> Pluto.run()
 ```
 
-Have a look at the FAQ:
-https://github.com/JuliaPluto/Pluto.jl/wiki
+Have a look at the documentation:
+https://plutojl.org/en/docs/
 """
 module Pluto
 
